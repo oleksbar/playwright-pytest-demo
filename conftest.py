fc9c50aa1361da54dc433a9f6a48350f888f5d5c
oleksbar <oleksbar1@gmail.com>
2025-05-24 16:06:00 +0200

    Added test: search_duck

--- conftest.py
+++ conftest.py
@@ -6,6 +6,10 @@ def browser():
     """Launch the browser once per test session."""
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    context.set_default_timeout(10000)
+    context.set_default_navigation_timeout(15000)
     yield browser
+    context.close()
     browser.close()
     playwright.stop()
